23caefb0dc02b03e327ecdca663e926d8bd93589
MOo207 <mohammedismaeal522@gmail.com>
2022-03-21 03:04:33 +0300

    initial commit

---
+++ StudentsApp/Models/StudentsContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+using TodoApi.Models;
+
+namespace StudentsApp.Models
+{
+    public class StudentsContext : DbContext
+    {
+        public StudentsContext(DbContextOptions<StudentsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> students { get; set; } = null!;
+    }
+}
